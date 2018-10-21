e3795c9774a9e5ea9307c4aa3f664243b120a2df
shikher06071994@gmail.com <shikher06071994@gmail.com>
2018-10-21 18:52:52 +0530

    Initial Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to Connect to Mongo DB Server because of: ' + err);
+    }
+    console.log("Connected to MongoDB @ Port 27017");
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').insertOne({
+        text: 'Something in the DB',
+        completed: 'false'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.collection('Users').insertOne({
+        name: "Shikher",
+        age: 24,
+        location: "India"
+    }, (err, result) => {
+        if (err) {
+           return console.log('Unable to insert into Users', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to Connect to Mongo DB Server because of: ' + err);
+    }
+    console.log("Connected to MongoDB @ Port 27017");
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }).catch((err) => {
+    //     console.log('Unable to Fetch Data due to: ' + err);
+    // });
+    db.collection('Todos').find().count().then((docs) => {
+        console.log('Todos', docs);
+        //console.log(JSON.stringify(docs, undefined, 2));
+    }).catch((err) => {
+        console.log('Unable to Fetch Data due to: ' + err);
+    });
+
+
+    //client.close();
+});
