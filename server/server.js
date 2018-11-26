09fbca71f99997574e9d85cd9cb1b6f23016ca5b
shikher0607 <shikher06071994@gmail.com>
2018-11-27 00:27:48 +0530

    Update by id/ patch using lodash

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectId} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 
@@ -25,29 +26,29 @@ app.post('/users', (req, res) => {
     });
 });
 
-// app.get('/users', (req, res) => {
-//     users.find().then((doc) => {
-//         res.send(doc);
-//     }).catch((err) =>{
-//         res.status(400).send(err);
-//     });
-// });
+app.get('/users', (req, res) => {
+    users.find().then((doc) => {
+        res.send(doc);
+    }).catch((err) =>{
+        res.status(400).send(err);
+    });
+});
 
-// app.get('/users/:id', (req, res) => {
-//     var id = req.params.id;
-//     if(!ObjectId.isValid(id)){
-//         return res.status(404).send();
-//     }
-//     users.findById(id).then((doc) => {
-//         if (!doc) {
-//             return res.status(404).send();
-//         }else{
-//             res.send({doc});
-//         }
-//     }).catch((err) => {
-//         res.status(400).send(err);
-//     });
-// });
+app.get('/users/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    users.findById(id).then((doc) => {
+        if (!doc) {
+            return res.status(404).send();
+        }else{
+            res.send({doc});
+        }
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
 
 app.delete('/users/:id', (req, res) => {
     var id = req.params.id;
@@ -62,6 +63,25 @@ app.delete('/users/:id', (req, res) => {
         }
     }).catch((err) => {
         res.status(400).send(err);
+    });
+});
+
+app.patch('/users/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['email']);
+    if(!ObjectId.isValid(id)){
+       return res.status(404).send();
+    }
+   // body.email = 'shikher@gmail.com';
+
+    users.findByIdAndUpdate(id, {$set: body}, {new: true}).then((doc) => {
+        if(!doc){
+            return res.status(404).send();
+        }else{
+            res.send({doc});
+        }
+    }).catch((err) => {
+        res.status(404).send(err);
     });
 });
 
